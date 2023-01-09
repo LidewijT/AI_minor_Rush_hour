26d768c8fe813c982845a1ac43e12ace5223e2c4
lidewij tijhuis <LidewijT>
2023-01-09 16:39:01 +0100

    created classes, gameboard dataframe and list of vehicles

--- lidewij.py
+++ lidewij.py
@@ -1,13 +1,17 @@
 """
 In this file we will simulate the game of rush hour
 """
+# import functions
+import pandas as pd
 
 # read csv
-
+gameboard_df = pd.read_csv("gameboards\Rushhour6x6_1.csv")
+print(gameboard_df)
 
 # initiate the classes
 class Vehicles():
-    def __init__(self, orientation, col, row, length):
+    def __init__(self, car, orientation, col, row, length):
+        self.car = car
         self.orientation = orientation
         self.col = col
         self.row = row
@@ -17,5 +21,10 @@ class Board():
     def __init__(self):
         self.vehicle_list = []
 
-    def add_vehicles(self, orientation, col, row, length):
-        vehicle_list.append(Vehicles(orientation, col, row, length))
+    def add_vehicles(self, car, orientation, col, row, length):
+        print("test")
+        self.vehicle_list.append(Vehicles(car, orientation, col, row, length))
+
+for vehicle in gameboard_df.iterrows():
+    print(vehicle[1]['car'], vehicle[1]['orientation'],vehicle[1]['col'],vehicle[1]['row'], vehicle[1]['length'])
+    Board().add_vehicles(vehicle[1]['car'], vehicle[1]['orientation'], vehicle[1]['col'], vehicle[1]['row'], vehicle[1]['length'])
