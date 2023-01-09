6e7e672fe22edc77b223cf8fc03cb1af61cff349
lidewij tijhuis <LidewijT>
2023-01-09 16:10:34 +0100

    Initiated a vehicles and board class

--- lidewij.py
+++ lidewij.py
@@ -4,4 +4,18 @@ In this file we will simulate the game of rush hour
 
 # read csv
 
-# classes aanmaken
+
+# initiate the classes
+class Vehicles():
+    def __init__(self, orientation, col, row, length):
+        self.orientation = orientation
+        self.col = col
+        self.row = row
+        self.length = length
+
+class Board():
+    def __init__(self):
+        self.vehicle_list = []
+
+    def add_vehicles(self, orientation, col, row, length):
+        vehicle_list.append(Vehicles(orientation, col, row, length))
