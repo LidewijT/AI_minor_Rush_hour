12ce0384baaea27c27a52a3469a7ce87c096f722
LidewijT <117070396+LidewijT@users.noreply.github.com>
2023-01-18 13:47:38 +0100

    added a progressbar

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import math
 import copy
+from tqdm import tqdm
 
 from code.classes import board, game
 from code.algorithms import randomise, randomise2
@@ -22,7 +23,7 @@ if __name__ == "__main__":
     # ---------- Solve by random car movements - Branch and Bound -------------
     nr_moves_to_solve = math.inf
 
-    for i in range(1000):
+    for i in tqdm(range(1000), desc="Solving boards…", ascii=False, ncols=75):
         test_game = game.Game(f"data/solutions/" + args.output_file, \
             copy.deepcopy(test_board), randomise.random_car_move, \
                 branch_and_bound=True, nr_moves_to_solve=nr_moves_to_solve)
