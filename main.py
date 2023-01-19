93c3d80d3c4fa805483ac3b2e94e6f25aa61bbb3
LidewijT <lidewijbtijhuis@gmail.com>
2023-01-19 10:58:13 +0100

    ran random for 6x6_2 approx half a milion times, fastest solve was in 84 moves

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import copy
 from tqdm import tqdm
 
 from code.classes import board, game
-from code.algorithms import randomise, randomise2, priority_red_car#, state_spaces
+from code.algorithms import randomise, randomise2, priority_red_car
 
 if __name__ == "__main__":
     # set-up parsing command line arguments
@@ -21,24 +21,24 @@ if __name__ == "__main__":
     test_board = board.Board(f"data/gameboards/" + args.input_file)
 
     # ---------- Solve by random car movements - Branch and Bound -------------
-    # nr_moves_to_solve = math.inf
+    nr_moves_to_solve = 108#math.inf
 
-    # for i in tqdm(range(100), desc="Solving boards…", ascii=False, ncols=75):
-        # test_board = board.Board(f"data/gameboards/" + args.input_file)
-    #     test_game = game.Game(f"data/solutions/" + args.output_file, \
-    #         copy.deepcopy(test_board), randomise.random_car_move, \
-    #             branch_and_bound=True, nr_moves_to_solve=nr_moves_to_solve)
-
-    #     nr_moves_to_solve = test_game.nr_moves_to_solve
-
-    # --------- Solve by priority red car and random car movements ------------
-    nr_moves_to_solve = math.inf
-
-    for i in tqdm(range(10), desc="Solving boards…", ascii=False, ncols=75):
+    for i in tqdm(range(600000), desc="Solving boards…", ascii=False, ncols=75):
         test_board = board.Board(f"data/gameboards/" + args.input_file)
-
         test_game = game.Game(f"data/solutions/" + args.output_file, \
-                test_board, priority_red_car.move_priority_red_car, \
-                    branch_and_bound=True, nr_moves_to_solve=nr_moves_to_solve)
+            copy.deepcopy(test_board), randomise.random_car_move, \
+                branch_and_bound=True, nr_moves_to_solve=nr_moves_to_solve)
 
         nr_moves_to_solve = test_game.nr_moves_to_solve
+
+    # --------- Solve by priority red car and random car movements ------------
+    # nr_moves_to_solve = 150 # math.inf
+    #
+    # for i in tqdm(range(100000), desc="Solving boards…", ascii=False, ncols=75):
+    #     test_board = board.Board(f"data/gameboards/" + args.input_file)
+    #
+    #     test_game = game.Game(f"data/solutions/" + args.output_file, \
+    #             copy.deepcopy(test_board), priority_red_car.move_priority_red_car, \
+    #                 branch_and_bound=True, nr_moves_to_solve=nr_moves_to_solve)
+    #
+    #     nr_moves_to_solve = test_game.nr_moves_to_solve
