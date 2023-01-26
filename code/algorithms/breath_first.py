688a18957b491d544051d6b4fb76422d1735f5fd
LidewijT <lidewijbtijhuis@gmail.com>
2023-01-26 12:21:21 +0100

    rewrote breath first to class for object oriented programming

--- code/algorithms/breath_first.py
+++ code/algorithms/breath_first.py
@@ -7,99 +7,176 @@ import queue
 import copy
 
 
-def breath_first_search(start_state):
-    # initiate a dataframe to append the made moves to
-    moves_df = pd.DataFrame(columns=['car name', 'move'])
+class Breath_first_search():
+    def __init__(self, start_state):
+        self.start_state = start_state
+        self.moves_df = pd.DataFrame(columns=['car name', 'move'])
 
-    # search breath first
-    child, children_parent_dict = search(start_state)
-    child_tuple = tuple([tuple(row) for row in child.occupation])
+        # start the algorithms
+        self.breath_first_search()
 
-    print(f"number: {len(children_parent_dict)}")
+    def breath_first_search(self):
+        self.children_parent_dict = {tuple([tuple(row) for row in self.start_state.occupation]): None}
 
-    # search back in dict to find all the moves made to get to winning state
-    while children_parent_dict[child_tuple] != None:
-        move, parent = children_parent_dict[child_tuple]
-        # print(parent)
-        child_tuple = parent
+        q = queue.Queue()
+        q.put(self.start_state)
 
-        moves_df = append_move_to_DataFrame_reversed(moves_df, move)
+        while not q.empty():
+            current_state = q.get()
 
-    return moves_df
+            if current_state.occupation[current_state.exit_tile] == current_state.red_car:
+                self.child_tuple = tuple([tuple(row) for row in current_state.occupation])
+                return
 
+            next_states_list = self.get_next_states(current_state)
 
-def search(start_state):
-    # dictionary with children as key and as value a tuple of (move, parent)
-    # where move is also a tuple of (vehicle, direction)
-    children_parent_dict = {tuple([tuple(row) for row in \
-    start_state.occupation]): None}
-
-    q = queue.Queue()
-    q.put(start_state)
-
-    while not q.empty():
-        current_state = q.get()
-
-        # check if winning condtion is reached
-        if current_state.occupation[current_state.exit_tile] \
-        == current_state.red_car:
-            return current_state, children_parent_dict
-
-        # get list of possible children current state can give
-        next_states_list, children_parent_dict = \
-        get_next_states(current_state, children_parent_dict)
-
-        # put these children states in the queue
-        for next_state in next_states_list:
-            q.put(next_state)
+            for next_state in next_states_list:
+                q.put(next_state)
 
 
-def get_next_states(current_state, children_parent_dict):
-    next_states = []
+    def get_next_states(self, current_state):
+        next_states = []
+        parent_state = copy.deepcopy(current_state)
+        parent_occupation_tuple = tuple([tuple(row) for row in parent_state.occupation])
+        free_row, free_col = parent_state.get_free_squares()
 
-    # save the parent state to recall back to
-    parent_state = copy.deepcopy(current_state)
+        direction_list = ["left", "right", "up", "down"]
 
-    # safe the parent occupation as a tuple so it is hashable
-    parent_occupation_tuple = tuple([tuple(row) for row in \
-    parent_state.occupation])
+        for free_tile_nbr in range(len(free_row)):
+            r, c = free_row[free_tile_nbr], free_col[free_tile_nbr]
 
-    # retrieve lists of the free rows and columns
-    free_row, free_col = parent_state.get_free_squares()
+            for direction in direction_list:
+                vehicle = current_state.car_move(direction, r, c)
 
-    direction_list = ["left", "right", "up", "down"]
+                if vehicle:
+                    child_occupation_tuple = tuple([tuple(row) for row in current_state.occupation])
 
-    # systimatically go trough the empty tiles
-    for free_tile_nbr in range(len(free_row)):
+                    if child_occupation_tuple not in self.children_parent_dict:
+                        next_states.append(current_state)
+                        self.children_parent_dict[child_occupation_tuple] = (vehicle.car, direction), parent_occupation_tuple
 
-        r, c = free_row[free_tile_nbr], free_col[free_tile_nbr]
+                    current_state = copy.deepcopy(parent_state)
 
-        for direction in direction_list:
-            # make movement with the given direction, returns None if invalid move
-            vehicle = current_state.car_move(direction, r, c)
-
-            if vehicle:
-                # convert occupation to tuple of tuples to make it hashable
-                child_occupation_tuple = tuple([tuple(row) for row in \
-                current_state.occupation])
-
-                # check if this state is unique (pruning)
-                if child_occupation_tuple not in children_parent_dict:
-                    # append child to next states list and children parent dict
-                    next_states.append(current_state)
-
-                    children_parent_dict[child_occupation_tuple] = \
-                    ((vehicle.car, direction), parent_occupation_tuple)
-
-                # reset state
-                current_state = copy.deepcopy(parent_state)
-
-    return next_states, children_parent_dict
+        return next_states
 
 
-def append_move_to_DataFrame_reversed(moves_df, move):
-    # append move to DataFrame in reverse order since you start at the end
-    move_df = pd.DataFrame([[move[0], move[1]]], columns=['car name', 'move'])
-    moves_df = pd.concat([move_df, moves_df])
+    def string_moves_together(self, child_tuple, self.children_parent_dict):
+        print(f"number: {len(self.children_parent_dict)}")
 
-    return moves_df
+        while self.children_parent_dict[self.child_tuple] != None:
+            move, parent = self.children_parent_dict[self.child_tuple]
+            self.child_tuple = parent
+
+            self.moves_df = self.append_move_to_DataFrame_reversed(move)
+
+        return self.moves_df
+
+
+    def append_move_to_DataFrame_reversed(self, move):
+        move_df = pd.DataFrame([[move[0], move[1]]], columns=['car name', 'move'])
+        self.moves_df = pd.concat([move_df, self.moves_df])
+
+
+
+
+# def Breath_first_search():
+#     def __init__(self, start_state):
+#
+#
+#
+#     def breath_first_search(self, start_state):
+#         # initiate a dataframe to append the made moves to
+#         moves_df = pd.DataFrame(columns=['car name', 'move'])
+#
+#         # search breath first
+#         child, children_parent_dict = search(start_state)
+#         child_tuple = tuple([tuple(row) for row in child.occupation])
+#
+#         print(f"number: {len(children_parent_dict)}")
+#
+#         # search back in dict to find all the moves made to get to winning state
+#         while children_parent_dict[child_tuple] != None:
+#             move, parent = children_parent_dict[child_tuple]
+#             # print(parent)
+#             child_tuple = parent
+#
+#             moves_df = append_move_to_DataFrame_reversed(moves_df, move)
+#
+#         return moves_df
+#
+#
+#     def search(self, start_state):
+#         # dictionary with children as key and as value a tuple of (move, parent)
+#         # where move is also a tuple of (vehicle, direction)
+#         children_parent_dict = {tuple([tuple(row) for row in \
+#         start_state.occupation]): None}
+#
+#         q = queue.Queue()
+#         q.put(start_state)
+#
+#         while not q.empty():
+#             current_state = q.get()
+#
+#             # check if winning condtion is reached
+#             if current_state.occupation[current_state.exit_tile] \
+#             == current_state.red_car:
+#                 return current_state, children_parent_dict
+#
+#             # get list of possible children current state can give
+#             next_states_list, children_parent_dict = \
+#             get_next_states(current_state, children_parent_dict)
+#
+#             # put these children states in the queue
+#             for next_state in next_states_list:
+#                 q.put(next_state)
+#
+#
+#     def get_next_states(self, current_state, children_parent_dict):
+#         next_states = []
+#
+#         # save the parent state to recall back to
+#         parent_state = copy.deepcopy(current_state)
+#
+#         # safe the parent occupation as a tuple so it is hashable
+#         parent_occupation_tuple = tuple([tuple(row) for row in \
+#         parent_state.occupation])
+#
+#         # retrieve lists of the free rows and columns
+#         free_row, free_col = parent_state.get_free_squares()
+#
+#         direction_list = ["left", "right", "up", "down"]
+#
+#         # systimatically go trough the empty tiles
+#         for free_tile_nbr in range(len(free_row)):
+#
+#             r, c = free_row[free_tile_nbr], free_col[free_tile_nbr]
+#
+#             for direction in direction_list:
+#                 # make movement with the given direction, returns None if invalid move
+#                 vehicle = current_state.car_move(direction, r, c)
+#
+#                 if vehicle:
+#                     # convert occupation to tuple of tuples to make it hashable
+#                     child_occupation_tuple = tuple([tuple(row) for row in \
+#                     current_state.occupation])
+#
+#                     # check if this state is unique (pruning)
+#                     if child_occupation_tuple not in children_parent_dict:
+#                         # append child to next states list and children parent dict
+#                         next_states.append(current_state)
+#
+#                         children_parent_dict[child_occupation_tuple] = \
+#                         ((vehicle.car, direction), parent_occupation_tuple)
+#
+#                     # reset state
+#                     current_state = copy.deepcopy(parent_state)
+#
+#         return next_states, children_parent_dict
+#
+#
+#     def append_move_to_DataFrame_reversed(self, moves_df, move):
+#         # append move to DataFrame in reverse order since you start at the end
+#         move_df = pd.DataFrame([[move[0], move[1]]], columns=['car name', 'move'])
+#         moves_df = pd.concat([move_df, moves_df])
+#
+#         return moves_df
