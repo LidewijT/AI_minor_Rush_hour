e3f4ca84ae20f3595e8b27890fdde3d404db63de
Judith <judith.atsma@student.uva.nl>
2023-01-31 17:39:37 +0100

    deleted useless files

--- blabla.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-
-df = pd.read_csv("data/gameboards_different_max_depth_DLS2.csv")
-sns.scatterplot(x='max_depth', y='number_of_states', hue='board', data=df)
-plt.show()
-plt.savefig('6x6_diff_max_depth.png')
